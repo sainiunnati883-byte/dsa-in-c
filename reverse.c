6ac15d82d2b2cc0abd2204d3c531a45c98d6c49e
Unnati Saini <sainiunnati883@gmail.com>
2026-02-04 20:43:53 +0530

    Reversed the elements of an array

---
+++ reverse.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Two-pointer approach
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
