19e4b491dc9f23781de5ed9b4b38ada9483d0c54
Unnati Saini <sainiunnati883@gmail.com>
2026-02-13 19:32:07 +0530

    Boundary-To-Center Matrix Walk

---
+++ matrixwalk.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int matrix[r][c];
+
+    // Read matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+
+    // Spiral traversal
+    while (top <= bottom && left <= right) {
+
+        // 1. Left to Right (top row)
+        for (int j = left; j <= right; j++) {
+            printf("%d ", matrix[top][j]);
+        }
+        top++;
+
+        // 2. Top to Bottom (right column)
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+
+        // 3. Right to Left (bottom row)
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                printf("%d ", matrix[bottom][j]);
+            }
+            bottom--;
+        }
+
+        // 4. Bottom to Top (left column)
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
