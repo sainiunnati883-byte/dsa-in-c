9bbef064c230746e15501b7837825f5be3530123
Unnati Saini <sainiunnati883@gmail.com>
2026-02-21 21:50:07 +0530

    Create and Traverse Singly Linked List

---
+++ singly.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0) return 0;
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;      // first node
+            temp = head;
+        } else {
+            temp->next = newNode;  // link new node
+            temp = newNode;
+        }
+    }
+
+    // Traverse and print
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
