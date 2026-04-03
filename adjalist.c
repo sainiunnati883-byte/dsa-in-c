c2bb564064f9ea379df0d01e5409cd6383b070bf
Unnati Saini <sainiunnati883@gmail.com>
2026-04-03 18:34:41 +0530

    Graph Representation Using Adjacency List

---
+++ adjalist.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node for adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Graph structure
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+};
+
+// Create new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Create graph
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    graph->adjLists = (struct Node**)malloc(vertices * sizeof(struct Node*));
+
+    for (int i = 0; i < vertices; i++)
+        graph->adjLists[i] = NULL;
+
+    return graph;
+}
+
+// Add edge
+void addEdge(struct Graph* graph, int u, int v) {
+    // Add v to u's list
+    struct Node* newNode = createNode(v);
+    newNode->next = graph->adjLists[u];
+    graph->adjLists[u] = newNode;
+
+    // Uncomment below for undirected graph
+    // newNode = createNode(u);
+    // newNode->next = graph->adjLists[v];
+    // graph->adjLists[v] = newNode;
+}
+
+// Print graph
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->numVertices; i++) {
+        struct Node* temp = graph->adjLists[i];
+        printf("%d: ", i);
+
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Graph* graph = createGraph(n);
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(graph, u, v);
+    }
+
+    printGraph(graph);
+
+    return 0;
+}
